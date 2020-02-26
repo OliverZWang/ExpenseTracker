b8bf094ac0816c4cce931fdca5f5d3cc5c0f2e70
i <i@iltc.io>
2020-02-26 11:16:25 -0500

    Added Privacy Policy Page and Webhook

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
+def main():
     return 'Hello, World!'
+
+
+@app.route('/privacy')
+def privacy():
+    return 'Privacy Policy will be posted here.'
+
+
+@app.route('/webhook')
+def webhook():
+    mode = request.args.get('hub.mode')
+    token = request.args.get('hub.verify_token')
+    challenge = request.args.get('hub.challenge', '')
+    
+    if mode and token:
+        if mode == 'subscribe' and token == os.environ.get('VERIFY_TOKEN', 'DEFAULT_VERIFY_TOKEN'):
+            print('WEBHOOK_VERIFIED')
+            
+            return challenge, 200
+        
+        else:
+            return '403 Forbidden', 403
+    
+    return 'Miss hub.mode or hub.verify_token', 403
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
