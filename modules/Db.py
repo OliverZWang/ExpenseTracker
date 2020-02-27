ab6531bc329f6262ef94d380313f409221dad058
i <i@iltc.io>
2020-02-26 20:30:05 -0500

    Added DB

---
+++ modules/Db.py
@@ -0,0 +1,13 @@
+import os
+import pymongo
+
+class Db:
+    def __init__(self):
+        db_uri = os.environ.get('DATABASE_URI', 'mongodb://127.0.0.1:27017/db')
+        
+        client = pymongo.MongoClient(db_uri)
+        
+        db = client.get_default_database()
+    
+    
+db = Db()
