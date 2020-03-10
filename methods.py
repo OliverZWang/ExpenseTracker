52cd474f106dcd11bba9afd407d4f94e6770b886
Glitch (chartreuse-mahogany-asiago) <none>
2020-03-10 00:20:48 +0000

    🗺🏖 Updated with Glitch

--- methods.py
+++ methods.py
@@ -1,13 +1,30 @@
 import Facebook
 
+
 def check_new_user(user, msg):
-
+	quick_replies = [
+	      {
+	        "content_type":"text",
+	        "title":"What is this?",
+	        "payload":"<POSTBACK_PAYLOAD>",
+	        # "image_url":""
+	      }
+	    ]
 	if user.user_status == "new":
-		Facebook.send_message(user.uid, "Welcome!")
-		return False
+		Facebook.send_message(user.uid, "Welcome!!!", quick_replies = quick_replies)
 
+		# Change Status from "New" to something else in DB
+		# TO DO
+		return False
 	else:
 		return True
+
+
+
+
+
+
+
 
 def catch_all(user, msg):
 
--- start.sh
+++ start.sh
@@ -1,4 +1,1 @@
-# For development use (simple logging, etc):
 python app.py
-# For production use: 
-# gunicorn server:app -w 1 --log-file -
