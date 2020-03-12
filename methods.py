374a901f4c70cffc62e37412851ed3b6b3981790
Alan Luo <i@iltc.io>
2020-03-12 15:26:30 -0400

    Fixed some style issue

--- methods.py
+++ methods.py
@@ -4,16 +4,16 @@ import re
 
 
 def check_new_user(user, webhookEvent):
-    quick_replies = [
-        {
-            "content_type":"text",
-            "title":"What is this?",
-            "payload":"<POSTBACK_PAYLOAD>",
-            # "image_url":""
-        }
-    ]
-
     if user.user_status == "new":
+        quick_replies = [
+            {
+                "content_type": "text",
+                "title": "What is this?",
+                "payload": "<POSTBACK_PAYLOAD>",
+                # "image_url":""
+            }
+        ]
+
         Facebook.send_message(user.uid, "Welcome!", quick_replies=quick_replies)
 
         # Change Status from "New" to something else in DB
@@ -26,16 +26,16 @@ def check_new_user(user, webhookEvent):
 
 
 def give_intro(user, webhookEvent):
-    quick_replies = [
-        {
-            "content_type":"text",
-            "title":"Let's Get Started! ",
-            "payload":"<POSTBACK_PAYLOAD>",
-            # "image_url":""
-        }
-    ]
-
     if webhookEvent['message']['text'].find('What is this') >= 0:
+        quick_replies = [
+            {
+                "content_type": "text",
+                "title": "Let's Get Started! ",
+                "payload": "<POSTBACK_PAYLOAD>",
+                # "image_url":""
+            }
+        ]
+
         Facebook.send_message(user.uid, "Here goes a Short Intro",
                               quick_replies=quick_replies)
         return False
@@ -44,40 +44,40 @@ def give_intro(user, webhookEvent):
 
 
 def get_length(user, webhookEvent):
-    quick_replies = [
-        {
-            "content_type":"text",
-            "title":"3 days",
-            "payload":"valid_len",
-            # "image_url":""
-        },
-        {
-            "content_type":"text",
-            "title":"5 days",
-            "payload":"valid_len",
-            # "image_url":""
-        },
-        {
-            "content_type":"text",
-            "title":"1 week",
-            "payload":"valid_len",
-            # "image_url":""
-        },
-        {
-            "content_type":"text",
-            "title":"2 weeks",
-            "payload":"valid_len",
-            # "image_url":""
-        },
-        {
-            "content_type":"text",
-            "title":"A Longer Time? ",
-            "payload":"valid_len",
-            # "image_url":""
-        }
-    ]
-
     if webhookEvent['message']['text'].lower().find('get started') >= 0:
+        quick_replies = [
+            {
+                "content_type": "text",
+                "title": "3 days",
+                "payload": "valid_len",
+                # "image_url":""
+            },
+            {
+                "content_type": "text",
+                "title": "5 days",
+                "payload": "valid_len",
+                # "image_url":""
+            },
+            {
+                "content_type": "text",
+                "title": "1 week",
+                "payload": "valid_len",
+                # "image_url":""
+            },
+            {
+                "content_type": "text",
+                "title": "2 weeks",
+                "payload": "valid_len",
+                # "image_url":""
+            },
+            {
+                "content_type": "text",
+                "title": "A Longer Time? ",
+                "payload": "valid_len",
+                # "image_url":""
+            }
+        ]
+
         Facebook.send_message(user.uid,
                               "How long is your next budget period?",
                               quick_replies=quick_replies)
@@ -87,40 +87,40 @@ def get_length(user, webhookEvent):
 
 
 def catch_long_request(user, webhookEvent):
-    quick_replies = [
-        {
-            "content_type":"text",
-            "title":"3 days",
-            "payload":"valid_len",
-            # "image_url":""
-        },
-        {
-            "content_type":"text",
-            "title":"5 days",
-            "payload":"valid_len",
-            # "image_url":""
-        },
-        {
-            "content_type":"text",
-            "title":"1 week",
-            "payload":"valid_len",
-            # "image_url":""
-        },
-        {
-            "content_type":"text",
-            "title":"2 weeks",
-            "payload":"valid_len",
-            # "image_url":""
-        },
-        {
-            "content_type":"text",
-            "title":"A Longer Time? ",
-            "payload":"valid_len",
-            # "image_url":""
-        }
-    ]
-
     if webhookEvent['message']['text'].lower().find("longer time") >= 0:
+        quick_replies = [
+            {
+                "content_type": "text",
+                "title": "3 days",
+                "payload": "valid_len",
+                # "image_url":""
+            },
+            {
+                "content_type": "text",
+                "title": "5 days",
+                "payload": "valid_len",
+                # "image_url":""
+            },
+            {
+                "content_type": "text",
+                "title": "1 week",
+                "payload": "valid_len",
+                # "image_url":""
+            },
+            {
+                "content_type": "text",
+                "title": "2 weeks",
+                "payload": "valid_len",
+                # "image_url":""
+            },
+            {
+                "content_type": "text",
+                "title": "A Longer Time? ",
+                "payload": "valid_len",
+                # "image_url":""
+            }
+        ]
+
         Facebook.send_message(user.uid,
                               "Nope",
                               quick_replies=quick_replies)
@@ -135,9 +135,9 @@ def ask_for_amount(user, webhookEvent):
         today = datetime.date.today()
         period = ''
         if length[1].find('week') >= 0:
-            period = datetime.timedelta(weeks = int(length[0]))
+            period = datetime.timedelta(weeks=int(length[0]))
         else:
-            period = datetime.timedelta(days = int(length[0]))
+            period = datetime.timedelta(days=int(length[0]))
         to_date = today + period
         # print(to_date)
         user.add_budgets(today, to_date, -1)
