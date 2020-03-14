8ef4e4813cc3467f5a24cf909310e6ff11b8545c
Alan Luo <i@iltc.io>
2020-03-14 18:00:51 -0400

    Fix a bug

--- utils.py
+++ utils.py
@@ -4,4 +4,4 @@ import datetime
 
 def debug(name, msg):
     if os.environ.get('IS_DEBUG', False):
-        print('[{} {}] {}'.format(datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f"), name, msg))
+        print('[{} {}] {}'.format(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f"), name, msg))
