11535779f1f83f27d7a27da0054098cddcaf92c5
Alan Luo <i@iltc.io>
2020-03-15 12:13:53 -0400

    Added Warning Log

--- utils.py
+++ utils.py
@@ -4,4 +4,8 @@ import datetime
 
 def debug(name, msg):
     if os.environ.get('IS_DEBUG', False):
-        print('[{} {}] {}'.format(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f"), name, msg))
+        print('[DEBUG {} {}] {}'.format(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f"), name, msg))
+
+
+def warning(name, msg):
+    print('[WARNING {} {}] {}'.format(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f"), name, msg))
